7ea8628ee1f47eb39dad11d811a15329ae81f7ac
YASH <yashsp1158@gmail.com>
2020-04-28 22:17:21 +0530

    Implemented Provider for state management

--- state_login/lib/main.dart
+++ state_login/lib/main.dart
@@ -1,47 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:state_login/login.dart';
-import 'package:state_login/signup.dart';
-import 'package:state_login/userrepo.dart';
+import 'package:state_login/apiprovider/user_api.dart';
+import 'package:state_login/homepage.dart';
+import 'package:state_login/pages/login_2.dart';
+import 'models/users.dart';
+import 'notifiers/auth_notifier.dart';
 
-void main()=>runApp(MyApp());
+// void main()=>runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
-  }
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (_) => UserRepo(),
+//       child: Consumer(
+//         builder: (context,UserRepo user,_){
+//           //switch case to display UI Pages according to states
+//           switch(user.status){
+//             case Status.Unintialized:
+//               return Splash();
+//             case Status.Unauthenticated:
+//             case Status.Authenticating:
+//               return LoginPage();
+//             case Status.signupauthenticating:
+//             case Status.notsignedup:
+//               return SignUpPage();
+//             case Status.signedup:
+//               return Task(id: user.id,);
+//             case Status.Authenticated:
+//               return Task(id: user.id);
+//           }
+//         } 
+//       ),
+//     );
+//   }
+// }
+
+
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          // builder: (context) => AuthNotifier(),
+          create: (BuildContext context) {
+            return AuthNotifier();
+          },
+        ),
+        // ChangeNotifierProvider(
+        //   builder: (context) => BookNotifier(),
+        //   create : (BuildContext context){
+        //     return BookNotifier();
+        //   },
+        // )
+      ],
+      child: TaskApp(),
+    ));
 }
 
-class HomePage extends StatelessWidget {
+class TaskApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => UserRepo(),
-      child: Consumer(
-        builder: (context,UserRepo user,_){
-          //switch case to display UI Pages according to states
-          switch(user.status){
-            case Status.Unintialized:
-              return Splash();
-            case Status.Unauthenticated:
-            case Status.Authenticating:
-              return LoginPage();
-            case Status.signupauthenticating:
-            case Status.notsignedup:
-              return SignUpPage();
-            case Status.signedup:
-              return Task(id: user.id,);
-            case Status.Authenticated:
-              return Task(id: user.id);
-          }
-        } 
+    return MaterialApp(     
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+            appBarTheme: AppBarTheme(
+              brightness: Brightness.light,
+              elevation: 5,
+              color: ThemeData.light().canvasColor,
+            )
+        ),
+      home: Consumer<AuthNotifier>(
+        builder: (context, notifier, child) {
+          return notifier.user != null ? Task(id: notifier.user,) : Login();
+        },
       ),
     );
   }
 }
+
 
 class Splash extends StatelessWidget {
   @override
@@ -54,20 +106,21 @@ class Splash extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
-  final id;
+// class Task extends StatelessWidget {
+//   final id;
 
-  const Task({Key key, this.id}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Tasks'),),
-      body: Column(
-        children: <Widget>[
-          Center(child:Text('$id')),
-          RaisedButton(onPressed: () => Provider.of<UserRepo>(context,listen: false).signOut(),)
-        ],
-      ),
-    );
-  }
-}
+//   const Task({Key key, this.id}) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     AuthNotifier auth=Provider.of<AuthNotifier>(context);
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Tasks'),),
+//       body: Column(
+//         children: <Widget>[
+//           Center(child:Text('$id')),
+//           RaisedButton(onPressed: () => signout(auth),)
+//         ],
+//       ),
+//     );
+//   }
+// }
