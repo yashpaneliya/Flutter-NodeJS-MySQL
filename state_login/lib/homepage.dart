c5a61d35a27f9e721467dd6dd6b4a452f398118a
YASH <yashsp1158@gmail.com>
2020-04-30 16:46:43 +0530

    Add self task and assigntask to other who are in your contacts and using this app

--- state_login/lib/homepage.dart
+++ state_login/lib/homepage.dart
@@ -1,7 +1,11 @@
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:state_login/apiprovider/user_api.dart';
 import 'package:state_login/contacts.dart';
+import 'package:state_login/models/assignTask.dart';
+import 'package:state_login/models/contactdetail.dart';
+import 'package:state_login/models/selftask.dart';
 import 'package:state_login/notifiers/auth_notifier.dart';
 
 class Task extends StatefulWidget {
@@ -16,12 +20,19 @@ class _TaskState extends State<Task> with SingleTickerProviderStateMixin {
   TabController tc;
   bool show=false;
   var sckey=GlobalKey<ScaffoldState>();
+  
+  List<ContactDetail> contactdetaillist;  //contacts list
+
   @override
   void initState() {
     super.initState();
+
     tc=TabController(length: 2,vsync:this);
-    refreshContacts();
+    
+    // contactdetaillist=refreshContacts();
+    // print(contactdetaillist);
   }
+
   @override
   Widget build(BuildContext context) {
     AuthNotifier auth=Provider.of<AuthNotifier>(context);
@@ -88,7 +99,9 @@ class _TaskState extends State<Task> with SingleTickerProviderStateMixin {
           ),
         ],),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {  },
+          onPressed: () { 
+            openselftasksheet(context,widget.id);
+           },
           child: Icon(Icons.add_alarm),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -112,7 +125,7 @@ class _TaskState extends State<Task> with SingleTickerProviderStateMixin {
                 setState(() {
                   sckey.currentState.openDrawer();
               });})),
-              Container(margin:EdgeInsets.only(right:10.0),child: IconButton(icon: Icon(Icons.account_circle), onPressed: (){}))
+              Container(margin:EdgeInsets.only(right:10.0),child: IconButton(icon: Icon(Icons.account_circle), onPressed: (){openOtherTaskSheet(context, widget.id);}))
             ],
           ),
         ),
@@ -120,5 +133,134 @@ class _TaskState extends State<Task> with SingleTickerProviderStateMixin {
     );
   }
 
+  void openselftasksheet(context,id){
+    Selftask st=Selftask();
+    st.id=id;
+    st.status=0;
+    final fkey=GlobalKey<FormState>();
+    showModalBottomSheet(
+      // isScrollControlled: true,
+      context: context, 
+      builder: (BuildContext bc){
+        return Form(
+          key: fkey,
+          child: Column(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Title'
+              ),
+              onChanged: (String val)=>st.title=val,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Description'
+              ),
+              onChanged: (String val)=>st.desc=val,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Date'
+              ),
+              onChanged: (String val)=>st.date=val,
+            ),
+            RaisedButton(
+              onPressed: ()async{
+                if(fkey.currentState.validate())
+                {
+                  if(await assignSelfTask(st)=='selftask added'){
+                    print('added');
+                  }
+                  else{
+                    print('cant add task');
+                  }
+                  Navigator.pop(context);
+                }
+              },
+            )
+          ],),
+        );
+      });
+  }
+
+  void openOtherTaskSheet(context,id){
+    var selectedContact;
+    Assigntask at=Assigntask();
+    at.id=id;
+    at.status=0;
+    final fkey=GlobalKey<FormState>();
+    showModalBottomSheet(
+      // isScrollControlled: true,
+      context: context, 
+      builder: (BuildContext bc){
+        return Form(
+          key: fkey,
+          child: Column(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Title'
+              ),
+              onChanged: (String val)=>at.title=val,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Description'
+              ),
+              onChanged: (String val)=>at.desc=val,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Date'
+              ),
+              onChanged: (String val)=>at.date=val,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 80.0,
+              child: FutureBuilder<List<ContactDetail>>(
+                future: refreshContacts(),
+                builder: (context,snapshot){
+                  if(snapshot.connectionState==ConnectionState.waiting)return Center(child: CircularProgressIndicator());
+                  else if(snapshot.hasData){
+                  return DropdownButton<String>(
+                    hint: Text('Select'),
+                    value: selectedContact,
+                    onChanged: (value){
+                      setState(() {
+                        selectedContact=value;
+                        at.tid=selectedContact;
+                        print("at.tid: ${at.tid}");
+                        print(selectedContact);
+                      });
+                    },
+                    items: snapshot.data.map((e) {
+                      print("snapshot.data.length ${snapshot.data.length}");
+                      return DropdownMenuItem<String>(child: Text(e.name),value: e.id,);
+                    }).toList(),
+                  );}
+                  else{
+                    return Container();
+                  }
+                },
+              ),
+            ),
+            RaisedButton(
+              onPressed: ()async{
+                // assignTaskToOther(at);
+                if(fkey.currentState.validate())
+                {
+                  if(await assignTaskToOther(at)=='task added'){
+                    print('added');
+                  }
+                  else{
+                    print('cant add task');
+                  }
+                  Navigator.pop(context);
+                }
+              },
+            )
+          ],),
+        );
+      });
+  }
 
 }
